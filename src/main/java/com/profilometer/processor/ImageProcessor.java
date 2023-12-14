7dcc4729b62c7fd9bdef9ea18e0b7d3345928cde
Rafael Rutsatz <rafael.rutsatz@domo.com>
2023-12-14 16:21:40 -0300

    feat: identify and extract axles ROI

--- src/main/java/com/profilometer/processor/ImageProcessor.java
+++ src/main/java/com/profilometer/processor/ImageProcessor.java
@@ -26,7 +26,8 @@ public class ImageProcessor {
 
     public void process(String imageFile, IntegerProperty heightProperty, boolean blur, int blurKernel,
                         boolean segmentation, int sobelKernel, double segmentationMinThreshold) {
-        // ### Original ###
+
+        // ### Original Image ###
         Mat src = Imgcodecs.imread(imageFile);
         if (src.empty()) {
             System.err.println("Cannot read image: " + imageFile);
@@ -37,20 +38,15 @@ public class ImageProcessor {
 
         // Extrair o chão
         try {
-            System.out.println("src.cols() " + src.cols() + " src.rows() " + src.rows());
             int threshold = 10; // limiar para identificar fim do chão
             double[] lastPixel = src.get(src.rows() - 1, 0); // inicia comparando o primeiro pixel
             int groundEnd = 0;
-            // find where the ground ends
-            for (int i = src.rows() - 2; i > 0; i--) { // percorre primeira coluna de baixo para cima
-                System.out.print("i " + i + ", ");
-                double[] pixel = src.get(i, 0); // BGR
 
-//                System.out.println("\tpixel " + pixel[0] + "\t " + pixel[1] + "\t " + pixel[2] + "\t " + (pixel[0] + pixel[1] + pixel[2]));
+            for (int i = src.rows() - 2; i > 0; i--) { // percorre primeira coluna de baixo para cima
+                double[] pixel = src.get(i, 0); // BGR
 
                 // Se o Blue ou o Green for maior que o threshold, indica o fim do chão
                 if (Math.abs(pixel[0] - lastPixel[0]) > threshold || Math.abs(pixel[1] - lastPixel[1]) > threshold) {
-                    System.out.println("======> Ground ended at " + i);
                     groundEnd = i;
                     break;
                 }
@@ -62,13 +58,10 @@ public class ImageProcessor {
             Mat groundImage = new Mat(src, groundRect);
             Window.addImage(groundImage, "Ground Removed", heightProperty);
 
-
             // Remover o chão da imagem original
             Rect rectNoGround = new Rect(0, 0, src.cols(), src.rows() - groundRect.height); // remover o chão
             Mat imageNoGround = new Mat(src, rectNoGround);
             Window.addImage(imageNoGround, "Image No Ground", heightProperty);
-
-
 
 
             // ### Gray ###
@@ -77,77 +70,109 @@ public class ImageProcessor {
             Window.addImage(srcGray, "Gray Image", heightProperty);
 
 
+            // ### Blur ###
+            Mat blurSrc = new Mat();
+            if (blur) {
+                Size blurKernelSize = new Size(blurKernel, blurKernel);
+                Imgproc.blur(srcGray, blurSrc, blurKernelSize);
+                Window.addImage(blurSrc, "Blur", heightProperty);
+            } else {
+                srcGray.copyTo(blurSrc);
+            }
+
+
             // ### Identificar fundo do veículo ###
+            int thresholdVehicle = 75; // limiar para identificar inicio do chão do veículo (Se for maior que threshold, é fundo)
+            double[] vehicleFloor = new double[blurSrc.cols()];
+            // find where the vehicle ground starts (i = column, j = row)
+            for (int i = 0; i <= blurSrc.cols() - 1; i++) { // percorre todas as colunas
+                for (int j = blurSrc.rows() - 2; j > 0; j--) { // percorre todas as linhas de baixo para cima
+                    double[] pixel = blurSrc.get(j, i); // Gray
 
-            for (int i = src.rows() - 2; i > 0; i--) { // percorre primeira coluna de baixo para cima
+                    // Se a escala de cinza for menor que threshold, indica inicio do objeto
+                    if (pixel[0] < thresholdVehicle) {
+                        vehicleFloor[i] = j;
+                        break;
+                    }
+                }
+            }
+
+            double vehicleFloorMean = 0;
+            Mat vehicleFloorEdgesImage = new Mat();
+            blurSrc.copyTo(vehicleFloorEdgesImage);
+
+            // Draw vehicle floor
+            for (int x = 0; x < vehicleFloor.length; x++) {
+                vehicleFloorMean += vehicleFloor[x];
+                Imgproc.line(vehicleFloorEdgesImage, new Point(x, vehicleFloor[x]), new Point(x, vehicleFloor[x]), new Scalar(255), 1);
+            }
+
+            // Calcula a média simples de todos os pontos, para identificar onde cortar a imagem.
+            // Daria para tentar fazer alguma espécia de normalização para melhorar a detecção de motos.
+            int vehicleFloorCutPoint = (int) vehicleFloorMean / vehicleFloor.length;
+
+            // Draw vehicle floor cut line
+            Imgproc.line(vehicleFloorEdgesImage, new Point(0, vehicleFloorCutPoint), new Point(vehicleFloor.length, vehicleFloorCutPoint), new Scalar(255), 1);
+            Window.addImage(vehicleFloorEdgesImage, "Vehicle Floor Edges", heightProperty);
 
 
+            // ### Crop Vehicle Axles ROI ###
+            Rect vehicleRect = new Rect(0, 0, vehicleFloorEdgesImage.cols(), vehicleFloorCutPoint); // recorta o chão
+            Mat imageVehicleNoAxles = new Mat(blurSrc, vehicleRect);
+            Window.addImage(imageVehicleNoAxles, "imageROI", heightProperty);
 
+            // Get the ROI with the axles
+            Rect vehicleAxlesROIRect = new Rect(0, vehicleRect.height, blurSrc.cols(), blurSrc.rows() - vehicleRect.height); // remover o chão
+            Mat vehicleAxlesImage = new Mat(blurSrc, vehicleAxlesROIRect);
+            Window.addImage(vehicleAxlesImage, "Vehicle Axles ROI", heightProperty);
+
+
+            // TODO Transformar as cores brancas em preto.
+
+
+            // ### Segmentation (Canny) ###
+            // https://opencv-java-tutorials.readthedocs.io/en/latest/07-image-segmentation.html#canny-edge-detector
+            Mat segmentedImage = new Mat();
+            if (segmentation) {
+                double minThreshold = segmentationMinThreshold;
+                double maxThreshold = minThreshold * 3; // Canny's recommendation
+                int sobelKernelSize = sobelKernel;
+                boolean useL2gradient = false;
+                Imgproc.Canny(vehicleAxlesImage, segmentedImage, minThreshold, maxThreshold, sobelKernelSize, useL2gradient);
+                Window.addImage(segmentedImage, "Segmentation", heightProperty);
+            } else {
+                imageVehicleNoAxles.copyTo(segmentedImage);
+            }
+
+
+            // ### Contours ###
+            // https://opencv-java-tutorials.readthedocs.io/en/latest/08-object-detection.html
+            Mat srcContours = new Mat(src.size(), 0);
+            Core.add(srcContours, Scalar.all(0), srcContours);
+
+            // init
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+
+            // find contours
+            Imgproc.findContours(segmentedImage, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            for (MatOfPoint point : contours) {
+//            System.out.println(point);
+            }
+
+            // if any contour exist...
+            if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
+                // for each contour, display it in blue
+                for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0]) {
+                    Imgproc.drawContours(srcContours, contours, idx, new Scalar(250, 0, 0));
+                }
+                Window.addImage(srcContours, "Contours", heightProperty);
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-//
-//        // ### Crop ###
-//        Rect roi = new Rect(0, srcGray.rows() / 2, srcGray.cols(), srcGray.rows() / 2);
-//        Mat croppedImage = new Mat(srcGray, roi);
-//        Window.addImage(croppedImage, "ROI Image", heightProperty);
-//
-//
-//        // ### Blur ###
-//        Mat blurSrc = new Mat();
-//        if (blur) {
-//            Size blurKernelSize = new Size(blurKernel, blurKernel);
-//            Imgproc.blur(croppedImage, blurSrc, blurKernelSize);
-//            Window.addImage(blurSrc, "Blur", heightProperty);
-//        } else {
-//            croppedImage.copyTo(blurSrc);
-//        }
-//
-//
-//        // ### Segmentation (Canny) ###
-//        // https://opencv-java-tutorials.readthedocs.io/en/latest/07-image-segmentation.html#canny-edge-detector
-//        Mat cannySrc = new Mat();
-//        if (segmentation) {
-//            double minThreshold = segmentationMinThreshold;
-//            double maxThreshold = minThreshold * 3; // Canny's recommendation
-//            int sobelKernelSize = sobelKernel;
-//            boolean useL2gradient = false;
-//            Imgproc.Canny(blurSrc, cannySrc, minThreshold, maxThreshold, sobelKernelSize, useL2gradient);
-//            Window.addImage(cannySrc, "Segmentation", heightProperty);
-//        } else {
-//            blurSrc.copyTo(cannySrc);
-//        }
-//
-//
-//        // ### Contours ###
-//        // https://opencv-java-tutorials.readthedocs.io/en/latest/08-object-detection.html
-//        Mat srcContours = new Mat(src.size(), 0);
-//        Core.add(srcContours, Scalar.all(0), srcContours);
-//
-//        // init
-//        List<MatOfPoint> contours = new ArrayList<>();
-//        Mat hierarchy = new Mat();
-//
-//        // find contours
-//        Imgproc.findContours(cannySrc, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
-//
-//        for (MatOfPoint point : contours) {
-////            System.out.println(point);
-//        }
-//
-//        // if any contour exist...
-//        if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
-//            // for each contour, display it in blue
-//            for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0]) {
-//                Imgproc.drawContours(srcContours, contours, idx, new Scalar(250, 0, 0));
-//            }
-//            Window.addImage(srcContours, "Contours", heightProperty);
-//        }
-
 
     }
 
